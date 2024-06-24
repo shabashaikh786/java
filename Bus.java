73aef5ebfa3bd08a5c459933ea114db34c1f5fc9
shabashaikh786 <shaba.xworkz@gmail.com>
2024-06-24 12:39:14 +0530

    this is my first commit

---
+++ Bus.java
@@ -0,0 +1,30 @@
+		public class Bus {
+	public static void main(String args[]){
+	
+	
+	String busName="city bus";
+	String busType="govt";
+	int ticketN0=1234;
+	int price =90;
+	String busNo="KA5678";
+	String source="gulberga";
+	String destination="pune";
+	int travelerAge=23;
+	String travelerName="sanag";
+	long distance=200;
+	
+	
+	if(price<100)
+	{
+				System.out.println("condition is true");
+
+	System.out.println("ticket price is :"  + price + "from :" +source+  "to :" +destination );
+	}
+	else{
+	System.out.println("condition is false");
+	
+	}
+	
+	
+	}
+}
---
+++ ChargerRunner.java
@@ -0,0 +1,21 @@
+public class ChargerRunner{
+
+	public static void main(String args[]){
+	
+	System.out.println("charger name is:" +Charger.chargerName);
+	System.out.println("no of charger are:" +Charger.noOfChargers);
+	System.out.println("chager voltage is:" +Charger.voltage);
+	System.out.println("chair weight  is:" +Charger.weight);
+	System.out.println("charger speed  is:" +Charger.speed);
+	System.out.println(" color is:" +Charger.color);
+	System.out.println("  is realme charger:" +Charger.isRealmeCharger);
+	System.out.println("  charger size:" +Charger.size);
+	System.out.println("charger name1  is:" +Charger.chargerName1);
+	System.out.println("charger price is:" +Charger.price);
+
+
+
+	
+	}
+
+}
